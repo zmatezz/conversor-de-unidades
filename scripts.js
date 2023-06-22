685a2085f65414d06f10cee3243dd0d01c566369
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-06-21 21:59:03 -0300

    assigning features

---
+++ scripts.js
@@ -0,0 +1,24 @@
+/* Selecionar os elementos */
+
+const inputElement = document.querySelector("#input")
+const fromElement = document.querySelector("#from")
+const toElement = document.querySelector("#to")
+const outputElement = document.querySelector("#output")
+const convertButton = document.querySelector("#convert-btn")
+const messageElement = document.querySelector("#message")
+
+/* função para converter as unidades */
+function convert (){
+  const fromValue = fromElement.value;
+  const toValue = toElement.value;
+  
+if(fromValue === toValue){
+  outputElement.value = inputElement.value;
+  messageElement.textContent = "";
+  return;
+}
+  
+  console.log(fromValue, toValue)
+}
+
+convertButton.addEventListener("click", convert)
