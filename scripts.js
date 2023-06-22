e46cf28c968fd424ea0f0165f2c431a158636ff6
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-06-21 22:13:44 -0300

    assigning features

--- scripts.js
+++ scripts.js
@@ -8,16 +8,37 @@ const convertButton = document.querySelector("#convert-btn")
 const messageElement = document.querySelector("#message")
 
 /* função para converter as unidades */
-function convert (){
+function convert() {
   const fromValue = fromElement.value;
   const toValue = toElement.value;
-  
-if(fromValue === toValue){
-  outputElement.value = inputElement.value;
-  messageElement.textContent = "";
-  return;
-}
-  
+
+  if (fromValue === toValue) {
+    outputElement.value = inputElement.value;
+    messageElement.textContent = "";
+    return;
+  }
+
+  /* Converter a entrada para metros */
+  let meters
+  switch (fromValue) {
+    case "m":
+      meters = inputElement.value
+      break
+
+    case "km":
+      meters = inputElement.value * 1000
+      break
+
+    case "cm":
+      meters = inputElement.value / 100
+      break
+
+    case "mm":
+      meters = inputElement.value / 1000;
+      break
+  }
+
+
   console.log(fromValue, toValue)
 }
 
